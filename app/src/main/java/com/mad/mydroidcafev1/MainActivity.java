14410121bb48490821941de888f418b3e933dcfd
Jim Aloo Juma <jimaloo43@gmail.com>
2022-07-20 10:00:24 +0300

    initial commit

---
+++ app/src/main/java/com/mad/mydroidcafev1/MainActivity.java
@@ -0,0 +1,74 @@
+package com.mad.mydroidcafev1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_activity_main);
+        //inflate your toolbar
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        //create an instance of the tab layout and add the tabs
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label_3));
+        //set the tab to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        //Use the pager adapter to manage screens
+        //create an instance of the view pager
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        //create an instance for the pager adapter
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        //set the adapter to the view pager
+        viewPager.setAdapter(pagerAdapter);
+        //set Listener for clicks
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                if(item.getItemId() == R.id.nav_about){
+
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+}
